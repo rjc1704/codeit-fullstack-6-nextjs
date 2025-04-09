ec5d76d921790e7b7d6f3f7223b084a81e29a021
Jecheon Ryu <rjc1704@gmail.com>
2025-04-10 02:03:22 +0900

    라우트 핸들러 실습준비 일부주석추가

--- src/app/breeds/[id]/csr-page.jsx
+++ src/app/breeds/[id]/csr-page.jsx
@@ -19,6 +19,9 @@ export default function CSRCatBreedPage() {
   useEffect(() => {
     async function loadCat() {
       try {
+        // TODO: 실습:
+        // 직접 api 함수 호출하는 대신
+        // /api/cat-breed-list/[id] 경로로 route handler 에서 호출되도록 구현해 보세요.
         const cats = await getCatByIdClient(id);
         setCat(cats[0]);
         setLoading(false);
--- src/app/breeds/[id]/page.jsx
+++ src/app/breeds/[id]/page.jsx
@@ -6,6 +6,10 @@ import BreedDetailHeader from "@/app/breeds/[id]/_components/BreedDetailHeader";
 export default async function CatBreedPage({ params }) {
   console.log("SSR-CatBreedPage");
   const { id } = await params;
+
+  // TODO: 실습:
+  // 직접 api 함수 호출하는 대신
+  // /api/cat-breed-list/[id] 경로로 route handler 에서 호출되도록 구현해 보세요.
   const cats = await getCatById(id);
   const cat = cats[0];
 
